af43a9da75f1368990a52f45c64edd8ca6807618
Emrah Duruer <emrahduruer@gmail.com>
2018-11-14 20:47:17 -0500

    Updating codacy key.

--- grunt/shell.js
+++ grunt/shell.js
@@ -6,7 +6,7 @@ module.exports = function() {
 
 	return {
 		coverage_report_js : {
-			command : 'cat <%= directories.today_reports %>/coverage/js/lcov.info | codacy-coverage --token 0783e50890184aceadf2bf13f75e157b'
+			command : 'cat <%= directories.today_reports %>/coverage/js/lcov.info | codacy-coverage --token 288505a10bd44aa396a8b09dcb4c9d8c'
 		},
 	};
 };
