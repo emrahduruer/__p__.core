635a18343b279a14c93c45139e23c16cf6297a79
Emrah Duruer <emrahduruer@gmail.com>
2018-11-14 21:08:38 -0500

    Fixing copy tasks.

--- grunt/copy.js
+++ grunt/copy.js
@@ -4,82 +4,24 @@
 module.exports = function() {
 
 	return {
-		js : {
-			files : [
-				{
-					cwd : '<%= directories.newpen_composer %>/assets/dev/js',
-					src : 'consts.js',
-					dest : '<%= directories.newpen_composer %>/assets/js/',
-					expand : true,
-				}
-			]
-		},
-
-		css : {
-			files : [
-				{
-					cwd : '<%= directories.newpen_composer %>/assets/dev/css',
-					src : '**',
-					dest : '<%= directories.newpen_composer %>/assets/css/',
-					expand : true,
-				},
-				{
-					cwd : 'node_modules/@fortawesome/fontawesome-free/css',
-					src : [ 'all.min.css', 'solid.min.css', 'brands.min.css', 'regular.min.css' ],
-					dest: '<%= directories.newpen_composer %>/assets/css/fontawesome',
-					expand : true
-				},
-				{
-					cwd : 'node_modules/@fortawesome/fontawesome-free/webfonts',
-					src : '**',
-					dest: '<%= directories.newpen_composer %>/assets/css/webfonts',
-					expand : true
-				}
-			]
-		},
-
 		dist : {
 			files : [
 				{
-					cwd : '<%= directories.newpen_composer %>',
-					src : [ 'newpen-composer.php' ],
-					dest: '<%= directories.today_dist %>',
-					expand : true
-				},
-				{
-					cwd : '<%= directories.npc_php %>',
-					src : [ '**/*.php' ],
-					dest: '<%= directories.today_dist %>/npc',
-					expand : true
-				},
-				{
-					cwd : '<%= directories.core_php %>',
-					src : [ '**/*.php' ],
-					dest: '<%= directories.today_dist %>/newpen',
-					expand : true
-				},
-				{
-					cwd : '<%= directories.composer %>',
-					src  : [ '**/*.php' ],
-					dest : '<%= directories.today_dist %>/vendor',
-					expand : true
-				},
-				{
 					cwd : '<%= directories.compiled.js %>',
 					src  : [ '**/*.js' ],
-					dest : '<%= directories.today_dist %>/assets/js',
+					dest : '<%= directories.today_dist %>/lib/js',
 					expand : true
 				},
 				{
 					cwd : '<%= directories.compiled.lib %>',
 					src  : [ '**/*.*' ],
-					dest : '<%= directories.today_dist %>/assets/lib',
+					dest : '<%= directories.today_dist %>/lib/lib',
 					expand : true
 				},
 				{
 					cwd : '<%= directories.compiled.css %>',
 					src  : [ '**/*.css' ],
-					dest : '<%= directories.today_dist %>/assets/css',
+					dest : '<%= directories.today_dist %>/lib/css',
 					expand : true
 				},
 				{
@@ -88,41 +30,7 @@ module.exports = function() {
 					dest : '<%= directories.today_dist %>/settings',
 					expand : true
 				},
-				// TODO
-				{
-					cwd 	: '<%= directories.newpen_composer %>',
-					src  	: [ 'test/web/*.*' ],
-					dest 	: '<%= directories.today_dist %>',
-					expand 	: true
-				},
 			]
 		},
-
-		'localdeploy' : {
-			files : [
-				{
-					cwd : '<%= directories.today_dist %>',
-					src : '**',
-					dest : '<%= directories.local_deploy_path %>',
-					expand : true,
-				},
-			]
-		},
-
-		artifact_reports: {
-			src : '<%= directories.reports %>/reports-build<%= pkg.buildnumber %>.zip',
-			dest: '<%= directories.artifacts %>/reports.zip'
-		},
-
-		artifact_dist: {
-			files : [
-				{
-					cwd : '<%= directories.deployment %>/build<%= pkg.buildnumber %>/',
-					src : '*.zip',
-					dest : '<%= directories.artifacts %>',
-					expand : true,
-				},
-			]
-		}
 	};
 };
